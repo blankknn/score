0dd86df5c0325b8e27ed16f261ebed1c06325f29
Kashif Sulehria <captainkashi4@gmail.com>
2023-07-28 20:57:56 +0500

    cool

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -72,14 +72,14 @@
                 <div class="col-span-full">
                   <label for="current-password" class="block text-sm font-medium leading-6 text-white">Email</label>
                   <div class="mt-2">
-                    <input v-model="updatedData.email" id="current-password" name="current_password" type="password" autocomplete="current-password" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
+                    <input v-model="updatedData.email" id="current-password" name="current_password" type="email" autocomplete="current-password" class="block w-full rounded-md border-0 px-2 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
                   </div>
                 </div>
 
                 <div class="col-span-full">
                   <label for="new-password" class="block text-sm font-medium leading-6 text-white">New password</label>
                   <div class="mt-2">
-                    <input v-model="updatedData.password" id="new-password" name="new_password" type="password" autocomplete="new-password" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
+                    <input v-model="updatedData.password" id="new-password" name="new_password" type="password" autocomplete="new-password" class="block w-full rounded-md border-0 bg-white/5 py-1.5 px-2 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
                   </div>
                 </div> 
               </div>
@@ -90,39 +90,7 @@
             </form>
           </div>
 
-          <div class="grid max-w-7xl grid-cols-1 mx-auto gap-x-8 gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
-            <div>
-              <h2 class="text-base font-semibold leading-7 text-white">Log out other sessions</h2>
-              <p class="mt-1 text-sm leading-6 text-gray-400">Please enter your password to confirm you would like to log out of your other sessions across all of your devices.</p>
-            </div>
-
-            <form class="md:col-span-2 pl-20">
-    <div class="grid grid-cols-1 gap-x-6 gap-y-8 sm:max-w-xl sm:grid-cols-6">
-      <!-- ... (previous code) -->
-
-      <div class="col-span-full">
-        <label for="email" class="block text-sm font-medium leading-6 text-white">Email address</label>
-        <div class="mt-2">
-          <!-- Bind the input field to updatedEmail -->
-          <input v-model="updatedData.updatedEmail" id="email" name="email" type="email" autocomplete="email" class="block px-2 text-white w-full rounded-md border-0 bg-white/5 py-1.5 shadow-sm ring-1 ring-inset px-4 ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
-        </div>
-      </div>
-
-      <div class="col-span-full">
-        <label for="new-password" class="block text-sm font-medium leading-6 text-white">New password</label>
-        <div class="mt-2">
-          <!-- Bind the input field to updatedPassword -->
-          <input v-model="updatedData.updatedPassword" id="new-password" name="new_password" type="email" autocomplete="new-password" class="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6" />
-        </div>
-      </div>
-    </div>
-
-    <div class="mt-8 flex">
-      <!-- Call handleUpdatedFunc when the "Save" button is clicked -->
-      <button @click="handleUpdatedFunc" type="button" class="rounded-md bg-primary px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary">Save</button>
-    </div>
-  </form>
-          </div>
+           
 
           <div class="grid max-w-7xl grid-cols-1 gap-x-8 mx-auto gap-y-10 px-4 py-16 sm:px-6 md:grid-cols-3 lg:px-8">
             <div>
